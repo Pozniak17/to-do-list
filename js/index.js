83f76ac468d3c7c876b28bd74cdf7f095f97a9f1
Yevhenii Pozniak <jekamanu@gmail.com>
2022-05-02 22:30:32 +0300

    completed to-do list, module 6

--- js/index.js
+++ js/index.js
@@ -6,19 +6,45 @@ const itemTemplate = ({ id, isDone, text }) =>
           <input type="checkbox" ${isDone ? "checked" : ""} />
           <span>${text}</span>
         </label>
+        <button>x</button>
       </li>`;
 
 //! Вихідні дані
-const items = [
-  { id: 1, text: "Масло", isDone: false },
-  { id: 2, text: "Сир", isDone: true },
-  { id: 3, text: "Ковбаса", isDone: false },
-  { id: 4, text: "Часник", isDone: true },
-  { id: 5, text: "Молоко", isDone: false },
+let items = [
+  { id: "1", text: "Масло", isDone: false },
+  { id: "2", text: "Сир", isDone: true },
+  { id: "3", text: "Ковбаса", isDone: false },
+  { id: "4", text: "Часник", isDone: true },
+  { id: "5", text: "Молоко", isDone: false },
 ];
 
 const refs = {
-  body: document.querySelector("body"),
+  ul: document.querySelector("ul"),
+  form: document.querySelector("form"),
+};
+
+const handleIsDoneChange = (event) => {
+  const parent = event.target.closest("li");
+  const { id } = parent.dataset;
+
+  items = items.map((item) =>
+    item.id === id
+      ? {
+          ...item,
+          isDone: !item.isDone,
+        }
+      : item
+  );
+
+  renderList();
+};
+
+const handleDeleteItem = (event) => {
+  const parent = event.target.closest("li");
+  const { id } = parent.dataset;
+
+  items = items.filter((item) => item.id !== id);
+  renderList();
 };
 
 const createForm = () => {
@@ -43,23 +69,66 @@ const createForm = () => {
   refs.body.append(form);
 };
 
-const createList = () => {
-  const ul = document.createElement("ul");
+const handleSubmit = (event) => {
+  event.preventDefault();
 
-  const list = items
-    .map(itemTemplate) //! в функцію передаємо функцію з 3 рядка
+  //! створюємо додавання елемента
+  const text = event.target.elements.text.value;
+  const newItem = {
+    id: Date.now().toString(),
+    text,
+    isDone: false,
+  };
 
-    //! Фіксимо коми(масив ставить все через коми), задаємо join без нічого.
-    .join("");
-  ul.insertAdjacentHTML("beforeend", list);
-  refs.body.append(ul);
+  items.push(newItem);
+  renderList();
+  //* reset скидує до стартового стану програми
+  refs.form.reset();
 };
 
-createForm();
-createList();
+//! ФУНКЦІЯ КОСТИЛЬ!
+//! кожного разу добавляємо addEventListeners()
+const addItemListeners = () => {
+  // checkboxes
+  const listItems = document.querySelectorAll('input[type="checkbox"]');
+
+  listItems.forEach((item) =>
+    item.addEventListener("change", handleIsDoneChange)
+  );
+
+  // delete buttons
+  const deleteButtons = document.querySelectorAll("li > button");
+
+  console.log(deleteButtons);
+
+  deleteButtons.forEach((button) =>
+    button.addEventListener("click", handleDeleteItem)
+  );
+};
+
+const renderList = () => {
+  const list = items
+    .map(itemTemplate) //! в функцію передаємо функцію з 3 рядка
+    //! Фіксимо коми(масив ставить все через коми), задаємо join без нічого.
+    .join("");
+
+  //! робимо очистку списку(бо він буде повторюватися)
+  refs.ul.innerHTML = "";
+  refs.ul.insertAdjacentHTML("beforeend", list);
+
+  addItemListeners();
+
+  // console.log(items);
+};
+
+refs.form.addEventListener("submit", handleSubmit);
+
+renderList();
 
 /*
  * 6 рядом, у інпута є атрибут checked значить він true,
  * якщо цього атрибута немає значить false.
  ! ${isDone ? "checked" : ""} якщо так то "checked", якщо ні, то ""
  * */
+
+//  1.35.00
